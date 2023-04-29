11276edd85163f1a4b406bcd4de265bb1e0a9e69
Patryk <sucheckipatryk@gmail.com>
2023-04-29 18:09:35 -0400

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+ for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+  
+    return gifts;
+  }
+  
+wrapGifts(gifts);
+
+const namesOnCard = ["Charlie", "Samip", "Patryk", "Ali"];
+const eventOnCard = ["Birthday"];
+
+function writeCards(namesOnCard, eventOnCard){
+    let messageHolder = [];
+    for (let i = 0; i < namesOnCard.length; i++) {
+        const message = (`Thank you, ${namesOnCard[i]}, for the wonderful ${eventOnCard} gift!`);
+        messageHolder.push(message)
+        }
+    return messageHolder;
+}
+
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
